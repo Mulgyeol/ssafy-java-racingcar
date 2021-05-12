42ff0bca6cde9ea8419ee9c47588632feb0f3e3c
Mulgyeol <mulgyeoljung@gmail.com>
2021-05-12 23:26:12 +0900

    feat:String클래스 split 테스트 구현

---
+++ src/test/java/step1/StringUtilsTest.java
@@ -0,0 +1,26 @@
+package step1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class StringUtilsTest {
+
+    @DisplayName("요구사항 1 : split 테스트")
+    @Test
+    void splitTest(){
+        //given
+        String sentence ="1,2";
+        String sentence2 = "1";
+
+        //when
+        String[] data = sentence.split(",");
+        String[] data2 = sentence2.split(",");
+
+        //then
+        assertThat(data).containsExactly("1","2");
+        assertThat(data2).containsExactly("1");
+    }
+
+}
