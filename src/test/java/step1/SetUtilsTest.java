d99bba3a10e0a14f9af477702c44cb8554060932
Mulgyeol <mulgyeoljung@gmail.com>
2021-05-13 00:07:40 +0900

    feat:Set Collection contains 테스트 구현

--- src/test/java/step1/SetUtilsTest.java
+++ src/test/java/step1/SetUtilsTest.java
@@ -3,6 +3,8 @@ package step1;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -34,5 +36,13 @@ class SetUtilsTest {
         //then
         assertThat(setSize).isEqualTo(size);
     }
+
+    @DisplayName("요구사항 2: contains 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void containsTest(int value) {
+        assertThat(numbers.contains(value)).isTrue();
+    }
+
 
 }
