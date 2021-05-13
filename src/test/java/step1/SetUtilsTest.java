20c41ed0cbaad41e8533195b7034799580eb1302
Mulgyeol <mulgyeoljung@gmail.com>
2021-05-14 00:17:25 +0900

    test: DisplayName 변경, ParameterizedTest name 부여

--- src/test/java/step1/SetUtilsTest.java
+++ src/test/java/step1/SetUtilsTest.java
@@ -26,7 +26,7 @@ class SetUtilsTest {
         numbers.add(3);
     }
 
-    @DisplayName("요구사항 1: size 테스트")
+    @DisplayName("요구사항 1: size() 메소드를 활용해 Set의 크기를 확인하는 테스트")
     @Test
     void sizeTest() {
         //given
@@ -39,15 +39,15 @@ class SetUtilsTest {
         assertThat(setSize).isEqualTo(size);
     }
 
-    @DisplayName("요구사항 2: contains 테스트")
-    @ParameterizedTest
+    @DisplayName("요구사항 2: contains() 메소드를 활용해 값이 존재하는지 확인하는 테스트")
+    @ParameterizedTest(name = "{index}. Set에 {0}이 존재하는지 확인하는 테스트")
     @ValueSource(ints = {1, 2, 3})
     void containsTest(int value) {
         assertThat(numbers.contains(value)).isTrue();
     }
 
-    @DisplayName("요구사항 3: contains false 테스트")
-    @ParameterizedTest
+    @DisplayName("요구사항 3: contains() 메소드를 활용해 값이 존재하면 True, 존재하지 않으면 False를 반환하는 테스트")
+    @ParameterizedTest(name = "{index}. contains() 메소드가 Set의 {0} 존재 여부를 제대로 반환하는지 확인하는 테스트")
     @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
     void containsFalseTest(int value, boolean result) {
         //when
